0f34f5400eb1f23adf397b0cf401f9f4fa407aba
Sergey Kh <falko.serega2016@yandex.ru>
2020-11-25 17:00:03 +0300

    the classes and handlers modules are finished

--- dist/script.js
+++ dist/script.js
@@ -95,27 +95,6 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-// (() => {
-//   const $ = function(selector) {
-//     const elements = document.querySelectorAll(selector);
-//     console.log(elements);
-//     const obj = {};
-//     obj.hide = () => {
-//       elements.forEach(elem => {
-//         elem.style.display = 'none';
-//       });
-//       return obj;
-//     };
-//     obj.show = () => {
-//       elements.forEach(elem => {
-//         elem.style.display = '';
-//       });
-//       return obj;
-//     };
-//     return obj;
-//   };
-//   window.$ = $;
-// })();
 const $ = function (selector) {
   return new $.prototype.init(selector);
 };
@@ -123,6 +102,12 @@ const $ = function (selector) {
 $.prototype.init = function (selector) {
   if (!selector) {
     return this; // {} - empty object
+  }
+
+  if (selector.tagName) {
+    this[0] = selector;
+    this.length = 1;
+    return this;
   }
 
   Object.assign(this, document.querySelectorAll(selector));
@@ -147,9 +132,101 @@ window.$ = $;
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./core */ "./src/js/lib/core.js");
 /* harmony import */ var _modules_display__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/display */ "./src/js/lib/modules/display.js");
+/* harmony import */ var _modules_classes__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/classes */ "./src/js/lib/modules/classes.js");
+/* harmony import */ var _modules_actions__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/actions */ "./src/js/lib/modules/actions.js");
+
+
 
 
 /* harmony default export */ __webpack_exports__["default"] = (_core__WEBPACK_IMPORTED_MODULE_0__["default"]);
+
+/***/ }),
+
+/***/ "./src/js/lib/modules/actions.js":
+/*!***************************************!*\
+  !*** ./src/js/lib/modules/actions.js ***!
+  \***************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.on = function (eventName, callback) {
+  if (!eventName || !callback) {
+    return this;
+  }
+
+  for (let i = 0; i < this.length; i++) {
+    this[i].addEventListener(eventName, callback);
+  }
+
+  return this;
+};
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.off = function (eventName, callback) {
+  if (!eventName || !callback) {
+    return this;
+  }
+
+  for (let i = 0; i < this.length; i++) {
+    this[i].removeEventListener(eventName, callback);
+  }
+
+  return this;
+};
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.click = function (handler) {
+  for (let i = 0; i < this.length; i++) {
+    if (handler) {
+      this[i].addEventListener('click', handler);
+    } else {
+      this[i].click();
+    }
+  }
+
+  return this;
+};
+
+/***/ }),
+
+/***/ "./src/js/lib/modules/classes.js":
+/*!***************************************!*\
+  !*** ./src/js/lib/modules/classes.js ***!
+  \***************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.addClass = function (...className) {
+  for (let i = 0; i < this.length; i++) {
+    this[i].classList.add(...className);
+  }
+
+  return this;
+};
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.removeClass = function (...className) {
+  for (let i = 0; i < this.length; i++) {
+    this[i].classList.remove(...className);
+  }
+
+  return this;
+};
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.toggleClass = function (className) {
+  for (let i = 0; i < this.length; i++) {
+    this[i].classList.toggle(className);
+  }
+
+  return this;
+};
 
 /***/ }),
 
@@ -218,7 +295,9 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.toggle = function () {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _lib_lib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lib/lib */ "./src/js/lib/lib.js");
 
-$('.active').toggle();
+$('button').on('click', function () {
+  $(this).toggleClass('active');
+});
 
 /***/ })
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 import './lib/lib';
 
-$('.active').toggle();
+$('button').on('click', function() {
+  $(this).toggleClass('active');
+});
+
