02f647bbe60996091609ac2d1d81ceabf6281fda
Sergey Kh <falko.serega2016@yandex.ru>
2020-11-25 19:50:41 +0300

    the actions module is finished

--- dist/script.js
+++ dist/script.js
@@ -222,6 +222,53 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.find = function (selecto
   return this;
 };
 
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.closest = function (selector) {
+  let counter = 0;
+
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i].closest(selector);
+    counter++;
+  }
+
+  const objLength = Object.keys(this).length;
+
+  for (; counter < objLength; counter++) {
+    delete this[counter];
+  }
+
+  return this;
+};
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.siblings = function () {
+  let numberOfItems = 0;
+  let counter = 0;
+  const copyObj = Object.assign({}, this);
+
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].parentNode.children;
+
+    for (let j = 0; j < arr.length; j++) {
+      if (copyObj[i] === arr[j]) {
+        continue;
+      }
+
+      this[counter] = arr[j];
+      counter++;
+    }
+
+    numberOfItems += arr.length - 1;
+  }
+
+  this.length = numberOfItems;
+  const objLength = Object.keys(this).length;
+
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
+
+  return this;
+};
+
 /***/ }),
 
 /***/ "./src/js/lib/modules/classes.js":
@@ -382,8 +429,10 @@ $('button').on('click', function () {
 }); // $('div').click(function() {
 //   console.log($(this).index());
 // });
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findme'));
 
-console.log($('div').eq(2).find('.some'));
+console.log($('.more').eq(0).siblings());
 
 /***/ })
 
--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,7 @@ $('button').on('click', function() {
 //   console.log($(this).index());
 // });
 
-console.log($('div').eq(2).find('.some'));
+// console.log($('div').eq(2).find('.some'));
+
+// console.log($('.some').closest('.findme'));
+console.log($('.more').eq(0).siblings());
