ab5af2e94224dc3bc8a53cee07667a10eeb44709
Sergey Kh <falko.serega2016@yandex.ru>
2020-11-26 20:47:29 +0300

    the dropdown menu is finished

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,18 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
   $('.w-500').fadeToggle(800);
 });
+
+$('.wrap').html(
+  `
+  <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" id="DropdownMenuButton">Тренировки</button>
+    <div class="dropdown-menu" data-toggle-id="DropdownMenuButton">
+      <a href="#" class="dropdown-item">Кросфит</a>
+      <a href="#" class="dropdown-item">Плавание</a>
+      <a href="#" class="dropdown-item">Футбол</a>
+    </div>
+  </div>
+  `
+);
+
+$('.dropdown-toggle').dropdown();
