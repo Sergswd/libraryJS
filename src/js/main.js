4437b1ed024ec49669fa2ac33aa5de23e2e699bb
Sergey Kh <falko.serega2016@yandex.ru>
2020-11-27 16:24:39 +0300

    the createModal method is added

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,35 @@ $('button').eq(2).on('click', () => {
   $('.w-500').fadeToggle(800);
 });
 
+$('#trigger').click(() => $('#trigger').createModal({
+  text: {
+    title: 'Modal title',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, dolorum natus temporibus soluta aperiam asperiores, inventore vero officiis cumque assumenda velit, aliquam amet nemo deserunt ducimus voluptatum voluptatem? Provident, illum!'
+  },
+  btns: {
+    count: 3,
+    settings: [
+      [
+        'Close',
+        ['btn-danger', 'mr-10'],
+        true
+      ],
+      [
+        'Save changes',
+        ['btn-success'],
+        false,
+        () => {
+          alert('Данные сохранены');
+        }
+      ],
+      [
+        'Alert',
+        ['btn-warning', 'ml-10'],
+        false,
+        () => {
+          alert('opened');
+        }
+      ]
+    ]
+  }
+}));
